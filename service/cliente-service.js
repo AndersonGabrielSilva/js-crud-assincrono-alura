1a32bd64050a7c06e80ce940754fe98a9b3a8ac9
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2021-10-30 15:09:54 -0300

    Promises

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,7 +1,60 @@
+const tabela = document.querySelector('[data-tabela]');
 
+const criaNovaLinha = (nome, email) => {
+    const linhaNovoCliente = document.createElemente('tr');
 
-const http = new XMLHttpRequest();
+    const conteudo = `<td class="td" data-td>${nome}</td>
+                      <td>${email}</td>
+                        <td>
+                            <ul class="tabela__botoes-controle">
+                                <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                                <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+                            </ul>
+                     </td>`;
 
-http.open('GET','http://localhost:300/profile')
+    linhaNovoCliente.innerHTML = conteudo;
 
-http.send()
+    return linhaNovoCliente;
+}
+
+const listaClientes = () => {
+
+    /*Promise :  Um Promise (de "promessa") representa um valor que pode estar disponível agora, 
+    no futuro ou nunca. ... */
+    const promise = new Promise((resolve, reject) => {
+        const http = new XMLHttpRequest();
+
+        http.open('GET', 'http://localhost:3000/profile');
+
+        //execulta quando o html é carregado
+        http.onload = () => {
+            if (http.status >= 400) {
+
+                //Se der erro passo pelo reject
+                reject(JSON.parse(http.response));
+
+            }
+            else {
+
+                // se der certo passo pelo resolve
+                resolve(JSON.parse(http.response));
+
+            }
+
+            http.send();
+        }
+    })
+
+    console.log(promise);
+    return promise;
+};
+
+listaClientes()
+.then(data => {
+    //A cada retorno da promise execulta esta funcao
+    data.forEach(elemento => {
+        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email));
+    });})//resolve
+.catch(error => {
+    
+})//reject
